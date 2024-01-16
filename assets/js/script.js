ab069e8e57c63f3a2be9a92612ed3d9ef524888c
elamont174 <>
2024-01-16 13:42:54 +0000

    Add JavaScript functionality to answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,11 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+
+})
 
 function startGame() {
     console.log('Started')
@@ -44,8 +49,35 @@ function resetState() {
     }
 }
 
-function selectAnswer(e) {}
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correctsetStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Retake quiz!'
+        startButton.classList.remove('hide')
+    }
+}
 
+//CHANGE RETAKE QUIZ BUTON TO GO TO FINAL FEEDBACK PAGE
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+           }
+    }
+    
+    function clearStatusClass(element) {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
+    }
 
 const questions = [
     {
