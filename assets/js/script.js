58c512ecce0077d8fa2d69f7b508050f99adf7f2
elamont174 <>
2024-01-16 13:21:34 +0000

    Add JavaScript functionality to questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,118 +1,231 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
-function setNextQuestion() {}
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-function selectAnswer() {}
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {}
 
 
 const questions = [
     {
         question: "What is the charge on an electron?",
-        answers: ['negative', 'positive', 'neutral', 'does not exist'],
-        correctAnswer: 'negative'
-  },
-    {
-        question: "What is the charge on a proton?",
-        answers: ['negative', 'positive', 'neutral', 'does not exist'],
-        correctAnswer: 'positive'
-},
-    {
-        question: "What is the charge on a neutron?",
-        answers: ['negative', 'positive', 'neutral', 'does not exist'],
-        correctAnswer: 'neutral'
-},
-{
-        question: "What is the mass on an electron?",
-        answers: ['0', '1', '12', '100'],
-        correctAnswer: '0'
+        answers: [
+            {text: 'negative', correct: true },
+            {text: 'positive', correct: false },
+            { text: 'neutral', correct: false },
+            { text: 'doesn\'t exist', correct: false }
+            ]
     },
-{
-        question: "What is the mass on a neutron?",
-        answers: ['0', '1', '12', '100'],
-        correctAnswer: '1'
-    },
-{
-        question: "What is the mass on a proton?",
-        answers: ['0', '1', '12', '100'],
-        correctAnswer: '1'
-    },
-{
-        question: "Where in an atom would you find an electron?",
-        answers: ['in shells', 'nucleus', 'you would not', 'neucleus'],
-        correctAnswer: 'in shells'
-},
-{
-        question: "Where in an atom would you find a neutron?",
-        answers: ['in shells', 'nucleus', 'you would not', 'neucleus'],
-        correctAnswer: 'nucleus'
-},
-{
-        question: "Where in an atom would you find a proton?",
-        answers: ['in shells', 'nucleus', 'you would not', 'neucleus'],
-        correctAnswer: 'nucleus'
-},
-{
-        question: "What is the chemical symbol for iron?",
-        answers: ['Ir', 'I', 'Fe', 'Fr'],
-        correctAnswer: 'Fe'
-    },
-{
-        question: "What is the chemical symbol for gold?",
-        answers: ['G', 'Go', 'Au', 'Ag'],
-        correctAnswer: 'Au'
-    },
-{
-        question: "What is the chemical symbol for mercury?",
-        answers: ['Hg', 'M', 'Me', 'Mr'],
-        correctAnswer: 'Hg'
-    },
-{
-        question: "What does the state symbol (s) mean?",
-        answers: ['solid', 'liquid', 'gas', 'aqueous'],
-        correctAnswer: 'solid'
-    },
-{
-        question: "What does the state symbol (l) mean?",
-        answers: ['solid', 'liquid', 'gas', 'aqueous'],
-        correctAnswer: 'liquid'
-    },
-{
-        question: "What does the state symbol (g) mean?",
-        answers: ['solid', 'liquid', 'gas', 'aqueous'],
-        correctAnswer: 'gas'
-    },
-{
-        question: "What does the state symbol (aq) mean?",
-        answers: ['solid', 'liquid', 'gas', 'aqueous'],
-        correctAnswer: 'aqueous'
-    },
-{
-        question: "On the Periodic Table, what is group 1 called?",
-        answers: ['alkali metals', 'alkaline earth metals', 'halogens', 'noble gases'],
-        correctAnswer: 'alkali metals'
-    },
-{
-        question: "On the Periodic Table, what is group 2 called?",
-        answers: ['alkali metals', 'alkaline earth metals', 'halogens', 'noble gases'],
-        correctAnswer: 'alkaline earth metals'
-    },
-{
-        question: "On the Periodic Table, what is group 7 called?",
-        answers: ['alkali metals', 'alkaline earth metals', 'halogens', 'noble gases'],
-        correctAnswer: 'halogens'
-    },
-{
-        question: "On the Periodic Table, what is group 0 called?",
-        answers: ['alkali metals', 'alkaline earth metals', 'halogens', 'noble gases'],
-        correctAnswer: 'noble gases'
-    },
-]
+        {
+            question: "What is the charge on a proton?",
+            answers: [
+                { text: 'negative', correct: false },
+                { text: 'positive', correct: true },
+                { text: 'neutral', correct: false },
+                { text: 'doesn\'t exist', correct: false }
+            ]
+        },
+        {
+            question: "What is the charge on a neutron?",
+            answers: [
+                { text: 'negative', correct: false },
+                { text: 'positive', correct: false },
+                { text: 'neutral', correct: true },
+                { text: 'doesn\'t exist', correct: false }
+            ]
+        },
+        {
+            question: "What is the mass of an electron?",
+            answers: [
+                { text: '0', correct: true },
+                { text: '1', correct: false },
+                { text: '12', correct: false },
+                { text: '100', correct: false }
+            ]
+        },
+        {
+            question: "What is the mass of a neutron?",
+            answers: [
+                { text: '0', correct: false },
+                { text: '1', correct: true },
+                { text: '12', correct: false },
+                { text: '100', correct: false }
+            ]
+        },
+        {
+            question: "What is the mass of a proton?",
+            answers: [
+                { text: '0', correct: false },
+                { text: '1', correct: true },
+                { text: '12', correct: false },
+                { text: '100', correct: false }
+            ]
+        },
+        {
+            question: "Where in an atom would you find an electron?",
+            answers: [
+                { text: 'in shells', correct: true },
+                { text: 'nucleus', correct: false },
+                { text: 'you wouldn\'t', correct: false },
+                { text: 'neucleus', correct: false }
+            ]
+        },
+        {
+            question: "Where in an atom would you find a neutron?",
+            answers: [
+                { text: 'in shells', correct: false },
+                { text: 'nucleus', correct: true },
+                { text: 'you wouldn\'t', correct: false },
+                { text: 'neucleus', correct: false }
+            ]
+        },
+        {
+            question: "Where in an atom would you find a proton?",
+            answers: [
+                { text: 'in shells', correct: false },
+                { text: 'nucleus', correct: true },
+                { text: 'you wouldn\'t', correct: false },
+                { text: 'neucleus', correct: false }
+            ]
+        },
+        {
+            question: "What is the chemical symbol for iron?",
+            answers: [
+                { text: 'Ir', correct: false },
+                { text: 'I', correct: false },
+                { text: 'Fe', correct: true },
+                { text: 'Fr', correct: false }
+            ]
+        },
+        {
+            question: "What is the chemical symbol for gold?",
+            answers: [
+                { text: 'G', correct: false },
+                { text: 'Go', correct: false },
+                { text: 'Au', correct: true },
+                { text: 'Ag', correct: false }
+            ]
+        },
+        {
+            question: "What is the chemical symbol for mercury?",
+            answers: [
+                { text: 'Hg', correct: true },
+                { text: 'M', correct: false },
+                { text: 'Me', correct: false },
+                { text: 'Mr', correct: false }
+            ]
+        },
+        {
+            question: "What does the state symbol (s) mean?",
+            answers: [
+                { text: 'solid', correct: true },
+                { text: 'liquid', correct: false },
+                { text: 'gas', correct: false },
+                { text: 'aqueous', correct: false }
+            ]
+        },
+        {
+            question: "What does the state symbol (l) mean?",
+            answers: [
+                { text: 'solid', correct: false },
+                { text: 'liquid', correct: true },
+                { text: 'gas', correct: false },
+                { text: 'aqueous', correct: false }
+            ]
+        },
+        {
+            question: "What does the state symbol (g) mean?",
+            answers: [
+                { text: 'solid', correct: false },
+                { text: 'liquid', correct: false },
+                { text: 'gas', correct: true },
+                { text: 'aqueous', correct: false }
+            ]
+        },
+        {
+            question: "What does the state symbol (aq) mean?",
+            answers: [
+                { text: 'solid', correct: false },
+                { text: 'liquid', correct: false },
+                { text: 'gas', correct: false },
+                { text: 'aqueous', correct: true }
+            ]
+        },
+        {
+            question: "On the Periodic Table, what is group 1 called?",
+            answers: [
+                { text: 'alkali metals', correct: true },
+                { text: 'alkaline earth metals', correct: false },
+                { text: 'halogens', correct: false },
+                { text: 'noble gases', correct: false }
+            ]
+        },
+        {
+            question: "On the Periodic Table, what is group 2 called?",
+            answers: [
+                { text: 'alkali metals', correct: false },
+                { text: 'alkaline earth metals', correct: true },
+                { text: 'halogens', correct: false },
+                { text: 'noble gases', correct: false }
+            ]
+        },
+        {
+            question: "On the Periodic Table, what is group 7 called?",
+            answers: [
+                { text: 'alkali metals', correct: false },
+                { text: 'alkaline earth metals', correct: false },
+                { text: 'halogens', correct: true },
+                { text: 'noble gases', correct: false }
+            ]
+        },
+        {
+            question: "On the Periodic Table, what is group 0 called?",
+            answers: [
+                { text: 'alkali metals', correct: false },
+                { text: 'alkaline earth metals', correct: false },
+                { text: 'halogens', correct: false },
+                { text: 'noble gases', correct: true }
+            ]
+        }
+    ];
